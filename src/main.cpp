5c535407f90d8d8c16569d5dd0337a83e2f8c82c
Ivan Miatselski <danorywork@gmail.com>
2026-05-21 22:31:38 +0300

    Setup initial enviroment with building sdl from source and linking it into an executable as a static library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include "iostream"
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
 
+int main(int argc, char** argv)
+{
+  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+  {
+    return -1;
+  }
 
-int main(){
-    std::cout << "Hello World!\n";
+  SDL_Quit();
+
+  return 0;
 }
