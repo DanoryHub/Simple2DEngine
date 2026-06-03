77a1245012c88f53e359e99dbad93db40d565af8
Ivan Miatselski <danorywork@gmail.com>
2026-06-03 20:55:48 +0300

    Started direct development of an game engine with a arcanoid game example

---
+++ src/engine/include/Engine/main_app.h
@@ -0,0 +1,39 @@
+﻿//
+// Created by IvanMiatselski on 02.06.2026.
+//
+
+#ifndef SIMPLE2DENGINE_MAIN_APP_H
+#define SIMPLE2DENGINE_MAIN_APP_H
+
+#include "SDL3/SDL.h"
+
+class MainApp {
+public:
+        MainApp(
+                const char* name, const char* version,
+                const char* identifier, const char* title,
+                int screenWidth = 1920, int screenHeight = 1080
+                );
+
+        ~MainApp();
+
+        SDL_AppResult Init(void **appstate, int argc, char *argv[]);
+
+        SDL_AppResult ProcessEvent(void *appstate, SDL_Event *event);
+
+        SDL_AppResult Iterate(void *appstate);
+
+        void Quit(void* appstate, SDL_AppResult result);
+
+private:
+        int appScreenWidth;
+        int appScreenHeight;
+
+        SDL_Window *mainWindow = nullptr;
+        SDL_Renderer *renderer = nullptr;
+
+        const char *appVersion, *appName, *appIdentifier;
+        const char *windowTitle;
+};
+
+#endif //SIMPLE2DENGINE_MAIN_APP_H
---
+++ src/examples/arcanoid/main.cpp
@@ -0,0 +1,43 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
+
+#include "Engine/main_app.h"
+
+MainApp *mainApp = new MainApp{"Arcanoid", "0.0.1", "arcanoid", "Arcanoid"};
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    if (!mainApp) {
+        SDL_Log("mainApp is not valid");
+        return SDL_APP_FAILURE;
+    }
+    return mainApp->Init(appstate, argc, argv);
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    if (!mainApp) {
+        SDL_Log("mainApp is not valid");
+        return SDL_APP_FAILURE;
+    }
+
+    return mainApp->ProcessEvent(appstate, event);
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    if (!mainApp) {
+        SDL_Log("mainApp is not valid");
+        return SDL_APP_FAILURE;
+    }
+
+    return mainApp->Iterate(appstate);
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    if (!mainApp) {
+        SDL_Log("mainApp is not valid");
+        return;
+    }
+
+    mainApp->Quit(appstate, result);
+}
