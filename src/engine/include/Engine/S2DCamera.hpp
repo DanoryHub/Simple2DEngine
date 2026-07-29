94a46a77ed17bc5748c022144a56b33b1c3a7dc7
Ivan Miatselski <danorywork@gmail.com>
2026-07-29 22:05:18 +0300

    Create base camera functions

--- src/engine/include/Engine/S2DCamera.hpp
+++ src/engine/include/Engine/S2DCamera.hpp
@@ -5,8 +5,18 @@
 #ifndef ARCANOID_S2DCAMERA_HPP
 #define ARCANOID_S2DCAMERA_HPP
 
+#include "Engine/S2DVector2.hpp"
 
 class S2DCamera {
+protected:
+    S2DVector2<float>* cameraPosition;
+
+public:
+    S2DCamera();
+    ~S2DCamera();
+
+    S2DVector2<float>* getCameraPosition() const;
+    void setCameraPosition(const S2DVector2<float>* cameraPosition);
 };
 
 
--- src/engine/src/Engine/S2DCamera.cpp
+++ src/engine/src/Engine/S2DCamera.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Engine/S2DCamera.hpp"
+
+
+S2DCamera::S2DCamera() {
+    cameraPosition = new S2DVector2<float>(0.f, 0.f);
+}
+
+S2DCamera::~S2DCamera() {
+    delete cameraPosition;
+}
+
+S2DVector2<float>* S2DCamera::getCameraPosition() const {
+    return cameraPosition;
+}
+
+void S2DCamera::setCameraPosition(const S2DVector2<float>* value) {
+    cameraPosition->x = value->x;
+    cameraPosition->y = value->y;
+}
