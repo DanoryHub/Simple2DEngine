41f8710e62b5aba7df7cfb9feafe02eea813194a
Ivan Miatselski <danorywork@gmail.com>
2026-08-18 21:41:34 +0300

    Add new interface for input receiver classes, just want to create pr for the branch :P :)

---
+++ src/engine/include/Engine/S2DIInputReceiver.hpp
@@ -0,0 +1,12 @@
+//
+// Created by ivan_miatselski on 2026-08-18.
+//
+
+#ifndef ENGINE_S2DIINPUTRECEIVER_HPP
+#define ENGINE_S2DIINPUTRECEIVER_HPP
+
+class S2DIInputReceiver {
+    virtual ~S2DIInputReceiver() = default;
+};
+
+#endif //ENGINE_S2DIINPUTRECEIVER_HPP
